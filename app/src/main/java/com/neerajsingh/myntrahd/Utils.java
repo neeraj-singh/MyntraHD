f2a7627f2244163f65fe3495d32f6bf3cb58c641
neeraj-singh <neeraj2raj@gmail.com>
2016-04-17 06:10:56 +0530

    some random thing

--- app/src/main/java/com/neerajsingh/myntrahd/Utils.java
+++ app/src/main/java/com/neerajsingh/myntrahd/Utils.java
@@ -5,68 +5,68 @@ package com.neerajsingh.myntrahd;
  */
 public class Utils {
     public static int getVideo(String videoCode) {
-        if(videoCode.equalsIgnoreCase("a")){
+        if(videoCode.equalsIgnoreCase("1.mp4")){
             return R.raw.a;
         }
 
-        if(videoCode.equalsIgnoreCase("b")){
+        if(videoCode.equalsIgnoreCase("2.mp4")){
             return R.raw.b;
         }
-        if(videoCode.equalsIgnoreCase("c")){
+        if(videoCode.equalsIgnoreCase("3.mp4")){
             return R.raw.c;
         }
-        if(videoCode.equalsIgnoreCase("d")){
+        if(videoCode.equalsIgnoreCase("4.mp4")){
             return R.raw.d;
-        }if(videoCode.equalsIgnoreCase("e")){
+        }if(videoCode.equalsIgnoreCase("5.mp4")){
             return R.raw.e;
         }
-        if(videoCode.equalsIgnoreCase("f")){
+        if(videoCode.equalsIgnoreCase("6.mp4")){
             return R.raw.f;
         }
-        if(videoCode.equalsIgnoreCase("g")){
+        if(videoCode.equalsIgnoreCase("7.mp4")){
             return R.raw.g;
         }
-        if(videoCode.equalsIgnoreCase("h")){
+        if(videoCode.equalsIgnoreCase("8.mp4")){
             return R.raw.h;
         }
-        if(videoCode.equalsIgnoreCase("i")){
+        if(videoCode.equalsIgnoreCase("9.mp4")){
             return R.raw.i;
         }
-        if(videoCode.equalsIgnoreCase("j")){
+        if(videoCode.equalsIgnoreCase("10.mp4")){
             return R.raw.j;
         }
         return R.raw.a;
     }
 
     public static int getProdImage(String videoCode) {
-        if(videoCode.equalsIgnoreCase("1")){
+        if(videoCode.equalsIgnoreCase("1.png")){
             return R.drawable.a;
         }
 
-        if(videoCode.equalsIgnoreCase("2")){
+        if(videoCode.equalsIgnoreCase("2.png")){
             return R.drawable.b;
         }
-        if(videoCode.equalsIgnoreCase("3")){
+        if(videoCode.equalsIgnoreCase("3.png")){
             return R.drawable.c;
         }
-        if(videoCode.equalsIgnoreCase("4")){
+        if(videoCode.equalsIgnoreCase("4.png")){
             return R.drawable.d;
-        }if(videoCode.equalsIgnoreCase("5")){
+        }if(videoCode.equalsIgnoreCase("5.png")){
             return R.drawable.e;
         }
-        if(videoCode.equalsIgnoreCase("6")){
+        if(videoCode.equalsIgnoreCase("6.png")){
             return R.drawable.f;
         }
-        if(videoCode.equalsIgnoreCase("7")){
+        if(videoCode.equalsIgnoreCase("7.png")){
             return R.drawable.g;
         }
-        if(videoCode.equalsIgnoreCase("8")){
+        if(videoCode.equalsIgnoreCase("8.png")){
             return R.drawable.h;
         }
-        if(videoCode.equalsIgnoreCase("9")){
+        if(videoCode.equalsIgnoreCase("9.png")){
             return R.drawable.i;
         }
-        if(videoCode.equalsIgnoreCase("10")){
+        if(videoCode.equalsIgnoreCase("10.png")){
             return R.drawable.j;
         }
         return R.drawable.a;
